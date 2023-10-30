2aa203c1e9f3e0c91be683b8b55df8124f6ee70a
asam268 <asam268@gmail.com>
2023-10-30 17:47:46 -0400

    Install Knex.js and create migrations

---
+++ db/knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+const path = require('path')
+
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'P@ssw0rd',
+      database: 'mydb'
+    },
+    migrations: {},
+    useNullAsDefault: true
+  }
+
+};
---
+++ db/migrations/20231030040225_users.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex, Promise) {
+    return knex.schema
+        .createTable('users', (table) => {
+            table.increments('id').primary().unsigned()
+            table.string('username').notNullable()
+            table.string('password').notNullable()
+            table.string('email').notNullable()
+            table.timestamp('created_at').nullable()
+        });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex, Promise) {
+    return knex.schema
+        .dropTable('users');
+};
