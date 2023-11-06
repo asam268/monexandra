f4a678706e144d34adfc5ab5053e2352c7021c58
asam268 <asam268@gmail.com>
2023-11-05 22:53:55 -0500

    use environment variables in server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const options = {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "P@ssw0rd",
-    database: "mydb",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 };
 
