55d3ce91e60f0d7dfc4bc0b90182b074a254bb10
asam268 <asam268@gmail.com>
2023-11-06 17:16:25 -0500

    add comments to recipes_routes.js

--- routes/recipes_routes.js
+++ routes/recipes_routes.js
@@ -4,24 +4,36 @@ const options = require(__dirname + "/./../db/knexfile");
 const knex = require("knex")(options.development);
 const Recipes = () => knex("recipes");
 
+// Route to get all recipes
 router.get("/", async (req, res) => {
   try {
+    // Fetch all recipes from the database
     const recipes = await Recipes().select("*");
+
+    // Send a successful response with the list of recipes
     res.status(200).send({ data: recipes });
   } catch (error) {
+    // Handle any errors that occur during the process
     console.error(error);
+
+    // Send an error response with a meaningful error message
     res.status(500).send({ error: "Internal Server Error" });
   }
 });
 
+// Route to create a new recipe
 router.post("/", async (req, res) => {
   try {
+    // Extract the required fields from the request body
     const { name, category, instructions, created_by } = req.body;
 
+    // Check if any required fields are missing
     if (!name || !category || !instructions || !created_by) {
+      // If so, send a bad request response with an error message
       return res.status(400).send({ error: "Missing required fields" });
     }
 
+    // Insert the new recipe into the database
     const recipe = await Recipes().insert({
       name,
       category,
@@ -29,9 +41,13 @@ router.post("/", async (req, res) => {
       created_by,
     });
 
+    // Send a successful response with the newly created recipe
     res.status(201).send({ data: recipe });
   } catch (error) {
+    // Handle any errors that occur during the process
     console.error(error);
+
+    // Send an error response with a meaningful error message
     res.status(500).send({ error: "Internal Server Error" });
   }
 });
